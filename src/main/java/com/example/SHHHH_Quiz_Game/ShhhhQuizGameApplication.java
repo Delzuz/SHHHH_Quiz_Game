7aad1eb5d4af263633b9de016c337345a35ddb2f
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-09-06 15:17:54 +0200

    Initial commit

---
+++ src/main/java/com/example/SHHHH_Quiz_Game/ShhhhQuizGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.SHHHH_Quiz_Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShhhhQuizGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShhhhQuizGameApplication.class, args);
+	}
+
+}
