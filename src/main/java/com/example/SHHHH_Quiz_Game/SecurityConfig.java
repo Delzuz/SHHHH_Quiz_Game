1be141fa67fd0ea760b8a03b27e11c6482ac8028
HenrikMacGregor <henrikmacgregor@gmail.com>
2022-09-07 10:51:59 +0200

    securityconfig

---
+++ src/main/java/com/example/SHHHH_Quiz_Game/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.SHHHH_Quiz_Game;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/start", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/start")
+                .defaultSuccessUrl("/userpage", true)
+                .permitAll();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
+        return manager;
+    }
+}
