83da40fc290a33005cb39383c452d157c1d9e1e2
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-09-07 10:33:40 +0200

    En kontroller klass med start och signup getmapping

---
+++ src/main/java/com/example/SHHHH_Quiz_Game/ShhhhController.java
@@ -0,0 +1,18 @@
+package com.example.SHHHH_Quiz_Game;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShhhhController {
+
+    @GetMapping("/")
+    public String start(){
+        return "start";
+    }
+
+    @GetMapping("/signup")
+    public String signup () {
+        return "create";
+    }
+}
