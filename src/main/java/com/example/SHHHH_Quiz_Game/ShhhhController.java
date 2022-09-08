4a533e4c83280685d07c69b1723ef50b6aa57f63
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-09-08 13:46:36 +0200

    massa ändringar

--- src/main/java/com/example/SHHHH_Quiz_Game/ShhhhController.java
+++ src/main/java/com/example/SHHHH_Quiz_Game/ShhhhController.java
@@ -5,13 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Random;
 
 @Controller
 public class ShhhhController {
 
     @Autowired
     UserRepository repository;
+    QuestionRepository qRepository;
 
     @GetMapping("/")
     public String start(){
@@ -47,4 +51,20 @@ public class ShhhhController {
     public String logout() {
         return "login";
     }
+
+    @GetMapping("/game")
+    public String startGame (Model model) {
+        QuestionRepository queRepository = new QuestionRepository();
+        Question question = queRepository.getRandomQuestion();
+        model.addAttribute("question", question);
+        Random random = new Random();
+        return switch (random.nextInt(1, 4)) {
+            case 1 -> "game1";
+            case 2 -> "game2";
+            case 3 -> "game3";
+            default -> "game";
+        };
+
+    }
+
 }
--- src/test/java/com/example/SHHHH_Quiz_Game/ShhhhQuizGameApplicationTests.java
+++ src/test/java/com/example/SHHHH_Quiz_Game/ShhhhQuizGameApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.SHHHH_Quiz_Game;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class ShhhhQuizGameApplicationTests {
