effbc52be975562ab729f5c43bb6d4fd0b473e1a
HenrikMacGregor <henrikmacgregor@gmail.com>
2022-09-09 14:51:51 +0200

    bakgrundbild på login-sidan mm.

--- src/main/java/com/example/SHHHH_Quiz_Game/SecurityConfig.java
+++ src/main/java/com/example/SHHHH_Quiz_Game/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/signup", "/style.css").permitAll()
+                .antMatchers("/signup", "/style.css", "/photos/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
