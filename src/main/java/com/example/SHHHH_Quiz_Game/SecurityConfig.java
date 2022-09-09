fed43f3685ccaa9f17378cae65bb8dc80b4e5317
HenrikMacGregor <henrikmacgregor@gmail.com>
2022-09-09 14:08:29 +0200

    liiite css

--- src/main/java/com/example/SHHHH_Quiz_Game/SecurityConfig.java
+++ src/main/java/com/example/SHHHH_Quiz_Game/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/signup").permitAll()
+                .antMatchers("/signup", "/style.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
